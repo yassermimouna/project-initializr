2ac2bb08f8c0267292a950dfee6efafd648667fb
yassermimouna <yasser.mimouna20@gmail.com>
2024-07-01 14:10:04 +0100

    initializ project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 const Welcome = () => {
   return (
     <div className=" w-full h-screen flex justify-center items-center text-center">
-      <div className="text-black text-5xl ">halo</div>
+      <div className="text-black text-5xl ">hello</div>
     </div>
   );
 };
